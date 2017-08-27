9d2b6046764c2665a244e3d53ed8c6b8157d3c95
ShirokoSama <141250108@smail.nju.edu.cn>
2017-08-27 16:09:46 +0800

    java

--- HostelWorld/src/main/java/nju/HostelWorldApplication.java
+++ HostelWorld/src/main/java/nju/HostelWorldApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HostelWorldApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HostelWorldApplication.class, args);
 	}
-
 }
